9e5d7eccec09c9a009ec2cccad17e4e18c82c981
MBUSI <mattm4300@gmail.com>
2016-11-15 15:32:42 -0600

    testbranchupdate

--- sc-vision/vision_v2/VisionResources/imageprocessing/scale_down_image.py
+++ sc-vision/vision_v2/VisionResources/imageprocessing/scale_down_image.py
@@ -2,5 +2,7 @@ import PIL
 from PIL import Image
 from PIL.Image import LANCZOS
 
+# LANCZOS has the best scale-down image quality of the available
+# filters.
 def scale_down_image(image, size):
     return image.resize(size, resample = PIL.Image.LANCZOS)
