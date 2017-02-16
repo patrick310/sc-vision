d4856a1795b5a22970013034f4ad1a93519cd3aa
Patrick Weber <patrick310@gmail.com>
2017-02-16 18:44:21 +0000

    changed brightness

--- sc-vision/rpi-capture/AcquireImages_with_light_ring_FIXED.py
+++ sc-vision/rpi-capture/AcquireImages_with_light_ring_FIXED.py
@@ -11,7 +11,7 @@ import RPi.GPIO as GPIO
 sys.setrecursionlimit(1000000)
 
 resolution = (3280, 2464)
-brightness = 30
+brightness = 36
 cam = picamera.PiCamera()
 cam.resolution = resolution
 cam.brightness = brightness
