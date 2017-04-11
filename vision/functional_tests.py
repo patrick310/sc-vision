793873c4f75eb3bb5b3cc76563a708d2a584023d
Patrick Weber <patrick310@gmail.com>
2017-04-11 09:28:56 -0500

    Merged previous fixes to config and train

--- vision/functional_tests.py
+++ vision/functional_tests.py
@@ -1,17 +1,20 @@
 import unittest
-from .deploy.camera import TestCamera
+from deploy.camera import TestCamera
+from train.train_network import *
 import imghdr
+import numpy as np
 
 
 class ImagePreparationTest(unittest.TestCase):
 
     def test_can_generate_test_images(self):
         camera = TestCamera()
-        camera.capture()
-
-    def can_prepare_images_h5py(self):
-        print("Need to finish tests")
+        test_image_list = [camera.capture() for count in range(0,4)]
+        self.assertIn('PIL.Image', str(test_image_list))
+		
+    def test_can_prepare_images_h5py(self):
         #Prepares and checks image sets
+        None
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
