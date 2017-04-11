02a34c67fbc8bbdc82e82c885268ed5a39878359
Patrick Weber <patrick310@gmail.com>
2017-04-11 09:42:01 -0500

    updated gitignore

--- vision/functional_tests.py
+++ vision/functional_tests.py
@@ -1,6 +1,6 @@
 import unittest
 from deploy.camera import TestCamera
-from train.train_network import *
+#from train.train_network import *
 import imghdr
 import numpy as np
 
@@ -10,6 +10,7 @@ class ImagePreparationTest(unittest.TestCase):
     def test_can_generate_test_images(self):
         camera = TestCamera()
         test_image_list = [camera.capture() for count in range(0,4)]
+        test_image_list = ["a","forced fail"]
         self.assertIn('PIL.Image', str(test_image_list))
 		
     def test_can_prepare_images_h5py(self):
