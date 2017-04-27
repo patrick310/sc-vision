b94614c139b370edf9aa856bc963e8a118857d1e
Patrick Weber <patrick310@gmail.com>
2017-04-27 08:29:59 -0500

    Modified network structure to mirror Matt's old design

--- vision/train/vision_data_processor.py
+++ vision/train/vision_data_processor.py
@@ -95,15 +95,28 @@ class VisionDataProcessor():
             input_shape=input_shape))
             # now: model.output_shape == (None, 64, 32, 32)
         
+        model.add(Activation('relu'))
+        
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        
         model.add(Flatten())
         
-        model.add(Dense(32))
-        model.add(Dense(1))
+        model.add(Dense(75))
+        model.add(Dense(40))
+        model.add(Dense(10))
+        
+        model.add(Activation('relu'))
+        
+        model.add(Dropout({{uniform(0, 1)}}))
+        
+        model.add(configs.nb_classes))
+        
+        model.add(Activation('sigmoid'))
             
         if configs.print_summary:
             model.summary()
         
-        model.compile(optimizer='rmsprop',
+        model.compile(optimizer='categorical_crossentropy',
               loss='binary_crossentropy',
               metrics=['accuracy'])
               
