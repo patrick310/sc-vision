79f43be0d6de8605a6bb77823ba4826124472f9a
Patrick Weber <patrick310@gmail.com>
2017-05-18 22:24:14 -0500

    Added/configured to experiment with inception cross-training

--- vision/train/vision_data_processor.py
+++ vision/train/vision_data_processor.py
@@ -1,8 +1,14 @@
 from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Input
 from keras.models import Sequential
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.layers.convolutional import Conv2D
 from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.applications.inception_v3 import InceptionV3
+from keras.preprocessing import image
+from keras.models import Model
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras import backend as K
 import h5py
 import configs
 import numpy as np
@@ -32,7 +38,6 @@ class VisionDataProcessor():
         batch_size = configs.batch_size,
         color_mode = configs.color_mode,
         class_mode = configs.class_mode,
-        save_to_dir = 'visualize'
         )
         
         return generated_generator
@@ -86,7 +91,7 @@ class VisionDataProcessor():
         test_file.close()
         
     def create_simple_keras_model(self):
-        input_shape = (1, configs.img_width, configs.img_height)
+        input_shape = (3, configs.img_width, configs.img_height)
         
         model = Sequential()
          
@@ -128,13 +133,86 @@ class VisionDataProcessor():
             validation_data=self.validation_generator, 
             validation_steps=int(configs.nb_val_images/configs.batch_size)
             )
-            
+
+    def fit_inception_model(self):
+        self.inception_model.fit_generator(
+            self.train_generator,
+            steps_per_epoch=int(configs.nb_test_images/configs.batch_size),
+            epochs=configs.nb_epoch,
+            validation_data=self.validation_generator,
+            validation_steps=int(configs.nb_val_images/configs.batch_size)
+            )
+
+    def fit_inception_model_short(self):
+        self.inception_model.fit_generator(
+            self.train_generator,
+            steps_per_epoch=int(configs.nb_test_images/configs.batch_size),
+            epochs=5,
+            validation_data=self.validation_generator,
+            validation_steps=int(configs.nb_val_images/configs.batch_size)
+            )
+
     def save_trained_keras_model_to_file(self):
         self.model.save('model_output.h5')
+
+    def inception_cross_train(self):
+        # create the base pre-trained model
+        input_tensor = Input(shape=(configs.img_width, configs.img_height, 3))
+        #base_model = InceptionV3(input_tensor=input_tensor, weights='imagenet', include_top=True)
+        base_model = InceptionV3(weights='imagenet', include_top=False)
+
+        # add a global spatial average pooling layer
+        base_model.summary()
+        x = base_model.output
+        x = GlobalAveragePooling2D()(x)
+        # let's add a fully-connected layer
+        x = Dense(1024, activation='relu')(x)
+        # and a logistic layer -- let's say we have 200 classes
+        predictions = Dense(2, activation='sigmoid')(x)
+
+        # this is the model we will train
+        self.inception_model = Model(inputs=base_model.input, outputs=predictions)
+
+        # first: train only the top layers (which were randomly initialized)
+        # i.e. freeze all convolutional InceptionV3 layers
+        for layer in base_model.layers:
+            layer.trainable = False
+
+        # compile the model (should be done *after* setting layers to non-trainable)
+        self.inception_model.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy')
+
+        # train the model on the new data for a few epochs
+        self.fit_inception_model_short()
+
+        # at this point, the top layers are well trained and we can start fine-tuning
+        # convolutional layers from inception V3. We will freeze the bottom N layers
+        # and train the remaining top layers.
+
+        # let's visualize layer names and layer indices to see how many layers
+        # we should freeze:
+        for i, layer in enumerate(base_model.layers):
+            print(i, layer.name)
+
+        # we chose to train the top 2 inception blocks, i.e. we will freeze
+        # the first 172 layers and unfreeze the rest:
+        for layer in model.layers[:172]:
+            layer.trainable = False
+        for layer in model.layers[172:]:
+            layer.trainable = True
+
+        # we need to recompile the model for these modifications to take effect
+        # we use SGD with a low learning rate
+        from keras.optimizers import SGD
+        model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy')
+
+        # we train our model again (this time fine-tuning the top 2 inception blocks
+        # alongside the top Dense layers
+        self.fit_inception_model()
         
 if __name__ == '__main__':
     dataprocessor = VisionDataProcessor()
     dataprocessor.create_simple_keras_model()
-    dataprocessor.fit_simple_keras_model()
-    dataprocessor.save_trained_keras_model_to_file()
+    dataprocessor.inception_cross_train()
+    #dataprocessor.fit_simple_keras_model()
+    #dataprocessor.save_trained_keras_model_to_file()
     print("Done.")
