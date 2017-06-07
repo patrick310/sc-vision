bb552ec6ec8e45a885651215e403249c89df08eb
Patrick Weber <patrick310@gmail.com>
2017-06-07 08:43:52 -0500

    more attempt

--- vision/train/data.py
+++ vision/train/data.py
@@ -5,8 +5,8 @@ import numpy as np
 from keras.utils.np_utils import to_categorical
 
 def load_inria_person(path):
-    pos_path = os.path.join(path, "/pos")
-    neg_path = os.path.join(path, "/neg")
+    pos_path = os.path.join(path, "pos")
+    neg_path = os.path.join(path, "neg")
     pos_images = [cv2.resize(cv2.imread(x), (64, 128)) for x in glob.glob(pos_path + "/*.jpg")]
     pos_images = [np.transpose(img, (2, 0, 1)) for img in pos_images]
     neg_images = [cv2.resize(cv2.imread(x), (64, 128)) for x in glob.glob(neg_path + "/*.jpg")]
--- vision/train/grad_demo.py
+++ vision/train/grad_demo.py
@@ -3,8 +3,14 @@ from keras.models import load_model
 from vis.visualization import visualize_saliency
 import numpy as np
 from PIL import Image
+
+from keras.preprocessing.image import img_to_array
 import configs
-from gradcam import grad_activation_map
+
+from vis.utils import utils
+from vis.utils.vggnet import VGG16
+from vis.visualization import visualize_saliency
+from vis.visualization import visualize_cam
 
 def set_resolution(cap, x, y):
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, int(x))
@@ -18,8 +24,12 @@ def format_image_for_network(image):
     return np_frame
 
 
-model = load_model(configs.model_save_name)
+#model = load_model(configs.model_save_name)
+model = VGG16(weights='imagenet', include_top=True)
 
+layer_name = 'tiger'
+#layer_idx = [idx for idx, layer in enumerate(model.layers) if layer.name == layer_name][0]
+layer_idx = (1)
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
 
@@ -27,26 +37,32 @@ if vc.isOpened():  # try to get the first frame
     rval, frame = vc.read()
 else:
     rval = False
-    
-print("frame type", type(frame))
+
 
 counter = 0
-prediction = "debug"
 
 set_resolution(vc, 224, 224)
 
 while rval:
     rval, frame = vc.read()
-    oframe = frame.copy()
 
-    pred_class = np.argmax(model.predict(format_image_for_network(frame)))
-    image = visualize_saliency(
-        model=model,
-        layer_idx=[0],
-        filter_indices=[pred_class],
-        seed_img=format_image_for_network(frame))
-    cv2.imshow("preview", image)
-    print('grad image ', type(image))
+    #seed_img = utils.load_img(frame, target_size=(224, 224))
+    seed_img = frame
+    # Convert to BGR, create input with batch_size: 1, and predict.
+
+    bgr_img = utils.bgr2rgb(seed_img)
+
+    img_input = format_image_for_network(bgr_img)
+
+    pred_class = np.argmax(model.predict(img_input))
+
+    heatmap = visualize_cam(model, layer_idx, [pred_class], seed_img)
+
+    if show:
+        plt.axis('off')
+        plt.imshow(heatmap)
+        plt.title('Attention - {}'.format(utils.get_imagenet_label(pred_class)))
+        plt.show()
     key = cv2.waitKey(20)
     if key == 27:  # exit on ESC
         break
