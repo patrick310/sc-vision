b31e3573e4d5dfa4e644b8447f7f015aaffd2f02
Patrick Weber <patrick310@gmail.com>
2017-06-18 20:44:25 -0500

    Fixed out setup.py template

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='vdp',
     version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    description='Vision Data Processor for training image classifiers',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='Patrick D. Weber',
+    author_email='patrick310@gmail.com',
+    url='https://github.com/patrick310/sc-vision',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
