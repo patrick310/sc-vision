ffb5d7ae5742c44687e22c4f7bfc24f9f19f3b65
Patrick Weber <patrick310@gmail.com>
2017-07-16 13:08:12 -0500

    Created core test; failing check for config generator

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from .context import sample
-
 import unittest
+from vdp.vision_data_processor import VisionDataProcessor
+from vdp.utils import create_model_config
 
 
 class BasicTestSuite(unittest.TestCase):
@@ -10,6 +10,11 @@ class BasicTestSuite(unittest.TestCase):
 
     def test_absolute_truth_and_meaning(self):
         assert True
+
+    def test_build_and_fit_model(self):
+        dataprocessor = VisionDataProcessor()
+        dataprocessor.create_simple_categorical_model()
+        dataprocessor.fit_model()
 
 
 if __name__ == '__main__':
