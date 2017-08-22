17518cdb1e0fec4324267d8add77c6b93a0d8e09
Patrick Weber <patrick310@gmail.com>
2017-08-21 21:01:22 -0500

    Added test elephant

--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -19,11 +19,11 @@ class AdvancedTestSuite(unittest.TestCase):
         self.assertEqual(self.lvp.get_model_resolution(),(224, 224))
 
     def test_can_detect_an_elephant(self):
-        self.assertIn('African_elephant',self.lvp.predict_top_class(self.lvp.load_image('elephant.jpg')))
+        self.assertIn('African_elephant',self.lvp.predict_top_class(self.lvp.load_image('elephant.testjpg')))
 
     def test_can_set_and_trigger_alarm(self):
         self.lvp.set_alarm_case('African_elephant')
-        self.assertTrue(self.lvp.has_alarm(self.lvp.load_image('elephant.jpg')))
+        self.assertTrue(self.lvp.has_alarm(self.lvp.load_image('elephant.testjpg')))
 
 
 if __name__ == '__main__':
